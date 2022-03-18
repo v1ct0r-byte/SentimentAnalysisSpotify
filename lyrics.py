89430a1cd3ead2666d39f61da4d727510509a9f3
Víctor <garciavicval@gmail.com>
2022-03-18 11:39:53 +0100

    Nueva rama con mis llaves y mi usuario

--- lyrics.py
+++ lyrics.py
@@ -1,8 +1,5 @@
 import spotipy
 
-#export SPOTIPY_CLIENT_ID = '136c655a58a24f98a4bb9142c6925bd1'
-#export SPOTIPY_CLIENT_SECRET = '60ac28c38b36430ebb06ab53e1ba7cf9'
-
 from spotipy.oauth2 import SpotifyClientCredentials
 
 auth_manager = SpotifyClientCredentials()
--- spotify_api.py
+++ spotify_api.py
@@ -3,7 +3,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 import config
 
-
 client_credentials = SpotifyClientCredentials(client_id=config.spotify_key, client_secret=config.spotify_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials)
 
@@ -14,9 +13,10 @@ def get_playlist_tracks(playlist_id):
     return(sp.playlist_tracks(playlist_id))
 
 def main():
-    results = get_playlists_user('markettes99')
-    result_playlist = get_playlist_tracks(results['items'][0]['id'])
-    for res in results['items']:
+    #results = get_playlists_user('markettes99')
+    resultsVictor = get_playlists_user('garciavicval')
+    result_playlist = get_playlist_tracks(resultsVictor['items'][0]['id'])
+    for res in resultsVictor['items']:
         print(res['id'])
 
     for res in result_playlist['items']:
