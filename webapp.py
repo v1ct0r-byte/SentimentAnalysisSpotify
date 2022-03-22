a64e61f8dd23c36152da450a8f78440329864f91
aucejito <tomanotademicorreo@hotmail.com>
2022-03-22 15:14:23 +0100

    Radar chart

--- webapp.py
+++ webapp.py
@@ -1,8 +1,10 @@
 from click import option
 from dash import *
+from matplotlib.pyplot import figure
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
-import playlistsProcessing as pp
+import processing as pp
 import emotions as em
 
 user_df = pd.DataFrame()
@@ -38,6 +40,13 @@ app.layout = html.Div(children=[
         html.Label(children='Gráfica de sentimiento de la playlist seleccionada'),
         dcc.Graph(id='sentiment-graph'),
     ]),
+    html.Br(),
+    html.Div(children=[
+        html.Label(children='Características de canciones'),
+        dcc.Dropdown(id='song1'),
+        html.Button(id='reset-polar',children='Reset',n_clicks=0),
+        dcc.Graph(id='polar',figure=go.Figure())
+    ]),
 ])
 
 @app.callback(
@@ -67,16 +76,20 @@ def get_playlists(clicks,value):
 
 @app.callback(
     Output('slider','max'),
+    Output('song1','options'),
+    Output('song1','value'),
     Input('playlist','value'),
 )
 def get_songs(value):
     global user_df, songsList
     songs_df = user_df[user_df['PlaylistName']==value]
     songsList = []
+    options = []
     for index, row in songs_df.iterrows():
         songsList.append({'artist':row['ArtistName'],'song':row['SongName']})
+        options.append({'label':row['ArtistName'] + ' - ' + row['SongName'],'value':row['SongId']})
     
-    return len(songsList)
+    return len(songsList), options, options[0]['value']
     
     
 @app.callback(
@@ -91,11 +104,58 @@ def show_graph(nclicks, value):
     res_df = em.getLyricsInfo(songsList)
     #merged_df = user_df.merge(res_df,'left',left_on='SongName',right_on='title')
     res_df.to_csv('carlos2.csv',index='false')
+
     #merged_df.to_csv('carlos3.csv',index='false')
     max_range = max(res_df['positive'].max(),res_df['negative'].max())
     max_range *= 1.05
     fig = px.scatter(res_df,'positive', 'negative',range_x=[-0.01,max_range], range_y=[-0.01,max_range],symbol='artist',color="title")
     return fig
+
+# @app.callback(
+#     Output('polar','figure'),
+#     Input('song1','value')
+# )
+# def show_polar(value):
+#     data,tempo = pp.getAudioFeatures(value)
+#     fig = px.line_polar(data, r='r', theta='theta', line_close=True)
+#     fig.update_traces(fill='toself')
+#     return fig
+
+@app.callback(
+    Output('polar','figure'),
+    Input('song1','value'),
+    Input('reset-polar', 'n_clicks'),
+    State('polar','figure')
+)
+def show_polar(value, clicks, figure):
+    ctx = callback_context
+
+    if not ctx.triggered:
+        button_id = 'No clicks yet'
+    else:
+        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
+
+    if(button_id=='reset-polar'):
+        figure['data'] = []
+        return figure
+    else:
+        data,tempo = pp.getAudioFeatures(value)
+        print(figure)
+        if(figure['data']!=None):
+            figure['data'].append(go.Scatterpolar(r=data['r'],
+                theta=data['theta'],
+                fill='toself',
+                name=user_df.loc[user_df['SongId']==value, 'ArtistName'].iloc[0] + ' - ' + user_df.loc[user_df['SongId']==value, 'SongName'].iloc[0]
+            ))
+        # else:
+        #     figure['data']=[go.Scatterpolar(r=data['r'],
+        #         theta=data['theta'],
+        #         fill='toself',
+        #         name = user_df.loc[user_df['SongId']==value, 'SongName'].iloc[0]
+        #         #name=user_df[user_df['SongId']==value]['ArtistName'].item() + ' - ' + user_df[user_df['SongId']==value]['SongName'].item()
+        #     )]
+        return figure
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
