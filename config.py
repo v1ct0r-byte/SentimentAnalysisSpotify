c87e6836b2f9d9c30b5e68b3426f0cc93475419a
Víctor García Valero <garciavicval@gmail.com>
2022-05-10 11:51:28 +0200

    Probando a ocultar las claves

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+#Marcos
+#spotify_key = "b9a380ccc7ea40698198008e22cc99da"
+#spotify_secret = "98a4f5afcc3241eb9f8e978ae3ff7ae9"
 
+#Víctor
+spotify_key = '136c655a58a24f98a4bb9142c6925bd1'
+spotify_secret = '60ac28c38b36430ebb06ab53e1ba7cf9'
